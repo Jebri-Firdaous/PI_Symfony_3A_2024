710775656648b924405ffa92c94207f4e40d0013
ons26 <ons.benmaaouia@esprit.tn>
2024-04-14 02:26:00 +0100

    css 50percent

---
+++ src/Controller/SmsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Twilio\Rest\Client;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SmsController extends AbstractController
+{
+    #[Route('/sms', name: 'app_sms')]
+    public function sendSms(): Response
+    {
+        $sid = "ACd712af928ef38f3b4753d770276588a4";
+        $token = "db958c2c382e047a284d9a60b3876013";
+        $phone = "92701943";
+$firstname="ons";
+        $client = new Client($sid, $token);
+
+        $message = $client->messages->create(
+            "+216".$phone, // to
+            array(
+                "from" => "+12514514104", //modified 
+                "body" => "Hello" .$firstname.
+                " ".
+                "We're excited to let you know that your recent order has been shipped! Your package is on its way and should be arriving soon."
+            )
+        );
+        return $this->render('home/backhome.html.twig', [
+            'controller_name' => 'AcceuilController',
+        ]);
+}
+
+}
